392eda1317c35bbe0ea9f7acdca7472011969897
Gustavo Reinert <gustavo.reinert.do.amaral@gmail.com>
2020-05-26 01:33:00 -0300

    Telas divididas

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Appbar, TextInput, Button, IconButton, Colors } from 'react-native-pape
 import { StyleSheet, View } from 'react-native';
 import MapView from 'react-native-maps';
 
-export default class MyComponent extends React.Component {
+export default class patrulhaCanina extends React.Component {
 
   _foraBolsonaro = () => alert("#ForaBolsonaro");
 
@@ -16,28 +16,32 @@ export default class MyComponent extends React.Component {
             title="Patrulha da Aglomeração"
           />
         </Appbar.Header>
-        {/* BOTÃO CÂMERA */}
         <View style={styles.container}>
-          <IconButton style={styles.iconbutton}
-            icon="camera"
-            color={Colors.red500}
-            size={75}
-            onPress={this._foraBolsonaro}
-          />
-          {/* INPUT ENDEREÇO */}
-          <TextInput style={styles.input}
-            label='Endereço'
-          />
-          {/* MAP */}
-          <MapView style={styles.map}></MapView>
+          <View style={styles.containerButtons}>
+            <View style={styles.viewButtons}>
+              <View style={styles.viewButtonCamera}>
+                {/* BOTÃO CÂMERA */}
+                <IconButton icon="camera" color={Colors.blue500} size={75} onPress={this._foraBolsonaro} />
+              </View>
+              <View style={styles.viewButtonMap}>
+                {/* BOTÃO MAP */}
+                <IconButton icon="map-marker" color={Colors.blue500} size={75} onPress={this._foraBolsonaro} />
+              </View>
+            </View>
+          </View>
+          <View style={styles.viewCameraMap}>
+            <View style={styles.viewCamera}>
+              {/* IMAGEM */}
+            </View>
+            <View style={styles.viewMap}>
+              {/* MAP */}
+              <MapView style={styles.map}></MapView>
+            </View>
+          </View>
           {/* INPUT NÚMERO DE PESSOAS */}
-          <TextInput style={styles.input}
-            label='Número de pessoas'
-          />
+          <TextInput style={styles.input} label='Número de pessoas' />
           {/* BOTÃO ALERTAR AUTORIDADES */}
-          <Button mode="contained" onPress={this._foraBolsonaro} style={styles.button}>
-            Notificar Autoridade
-          </Button>
+          <Button mode="contained" onPress={this._foraBolsonaro} style={styles.button}>Notificar Autoridade</Button>
         </View>
       </>
     );
@@ -47,20 +51,66 @@ export default class MyComponent extends React.Component {
 const styles = StyleSheet.create({
   input: {
     width: "80%",
+    paddingHorizontal: 110
   },
+
   container: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  containerButtons: {
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row"
+  },
+
+  button: {
+    marginTop: 20,
+    width: "80%",
+    height: "10%",
     alignItems: "center",
     justifyContent: "center"
   },
-  iconbutton: {
-    marginTop: 10
-  },
-  button: {
-    marginTop: 20
-  },
-  map: {
-    margin: "5%",
+
+  viewCameraMap: {
+    margin: 20,
     width: "80%",
-    height: "40%"
-  }
+    height: "40%",
+    flexDirection: "row",
+  },
+
+  viewCamera: {
+    flex: 1,
+    width: "100%",
+    height: "100%"
+  },
+
+  viewMap: {
+    flex: 1,
+    width: "100%",
+    height: "100%"
+  },
+
+  map: {
+    width: "100%",
+    height: "100%"
+  },
+
+  viewButtons: {
+    flexDirection: "row",
+    width: "80%"
+  },
+
+  viewButtonCamera: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+
+  viewButtonMap: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
 }) 
