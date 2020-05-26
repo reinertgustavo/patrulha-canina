f98cad820597d6d5842822f923ce0b8688e93196
Reinert <gustavo.reinert@thomsonreuters.com>
2020-05-26 00:17:39 -0300

    Primeira parte concluída

--- App.js
+++ App.js
@@ -1,19 +1,66 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Appbar, TextInput, Button, IconButton, Colors } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import MapView from 'react-native-maps';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class MyComponent extends React.Component {
+
+  _foraBolsonaro = () => alert("#ForaBolsonaro");
+
+  render() {
+    return (
+      <>
+        {/* HEADER DO TOPO */}
+        <Appbar.Header>
+          <Appbar.Content
+            title="Patrulha da Aglomeração"
+          />
+        </Appbar.Header>
+        {/* BOTÃO CÂMERA */}
+        <View style={styles.container}>
+          <IconButton style={styles.iconbutton}
+            icon="camera"
+            color={Colors.red500}
+            size={75}
+            onPress={this._foraBolsonaro}
+          />
+          {/* INPUT ENDEREÇO */}
+          <TextInput style={styles.input}
+            label='Endereço'
+          />
+          {/* MAP */}
+          <MapView style={styles.map}></MapView>
+          {/* INPUT NÚMERO DE PESSOAS */}
+          <TextInput style={styles.input}
+            label='Número de pessoas'
+          />
+          {/* BOTÃO ALERTAR AUTORIDADES */}
+          <Button mode="contained" onPress={this._foraBolsonaro} style={styles.button}>
+            Notificar Autoridade
+          </Button>
+        </View>
+      </>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    width: "80%",
   },
-});
+  container: {
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  iconbutton: {
+    marginTop: 10
+  },
+  button: {
+    marginTop: 20
+  },
+  map: {
+    margin: "5%",
+    width: "80%",
+    height: "40%"
+  }
+}) 
