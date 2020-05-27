105be44f358f385ea7667abe34f7882f8733048f
Gustavo Reinert <gustavo.reinert.do.amaral@gmail.com>
2020-05-26 23:02:39 -0300

    Update Request API

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
 import * as React from 'react';
 import { StyleSheet, View, Image } from 'react-native';
 import { Appbar, TextInput, Button, IconButton, Colors } from 'react-native-paper';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+import { getCurrentPositionAsync } from 'expo-location';
+import Axios from 'axios';
 
 export default class patrulhaCanina extends React.Component {
 
-  _foraBolsonaro = () => alert("#ForaBolsonaro");
+  constructor() {
+    super()
+    this.state = {
+      initialPosition: {
+        latitude: 48.8584,
+        longitude: 2.2945,
+        latitudeDelta: 0.05,
+        longitudeDelta: 0.05,
+      },
+    }
+  }
+
+  foraBolsonaro = () => alert("#ForaBolsonaro");
+
+  setLocation = () => {
+    getCurrentPositionAsync({
+      enableHighAccuracy: true
+    }).then((coords) => {
+      this.setState({
+        initialPosition: {
+          latitude: coords.coords.latitude,
+          longitude: coords.coords.longitude,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05
+        }
+      })
+    })
+  }
+
+  sendReport = () => {
+    Axios.post("http://patrulhaaglomeracao.com.br/denuncia", {})
+      .then(() => {
+        this.foraBolsonaro
+      })
+  }
 
   render() {
     return (
@@ -20,11 +56,11 @@ export default class patrulhaCanina extends React.Component {
             <View style={styles.viewButtons}>
               <View style={styles.viewButtonCamera}>
                 {/* BOTÃO CÂMERA */}
-                <IconButton icon="camera" color={Colors.blue500} size={75} onPress={this._foraBolsonaro} />
+                <IconButton icon="camera" color={Colors.blue500} size={75} onPress={this.foraBolsonaro} />
               </View>
               <View style={styles.viewButtonMap}>
                 {/* BOTÃO MAP */}
-                <IconButton icon="map-marker" color={Colors.blue500} size={75} onPress={this._foraBolsonaro} />
+                <IconButton icon="map-marker" color={Colors.blue500} size={75} onPress={this.setLocation} />
               </View>
             </View>
           </View>
@@ -35,13 +71,16 @@ export default class patrulhaCanina extends React.Component {
             </View>
             <View style={styles.viewMap}>
               {/* MAP */}
-              <MapView style={styles.map} />
+              <MapView style={styles.map}
+                region={this.state.initialPosition}>
+                <Marker coordinate={this.state.initialPosition} />
+              </MapView>
             </View>
           </View>
           {/* INPUT NÚMERO DE PESSOAS */}
           <TextInput style={styles.input} label='Número de pessoas' />
           {/* BOTÃO ALERTAR AUTORIDADES */}
-          <Button mode="contained" onPress={this._foraBolsonaro} style={styles.button}>Notificar Autoridade</Button>
+          <Button mode="contained" onPress={this.sendReport} style={styles.button}>Notificar Autoridade</Button>
         </View>
       </>
     );
